4fe96aa3fce9e86ce14390f1862fd41c6618f0d9
zahdoma <ben.rjh@Hotmail.com>
2024-12-08 11:58:57 -0700

    test.js changes

--- test.js
+++ test.js
@@ -1,24 +1,17 @@
-const { Connection, PublicKey, SystemProgram, Keypair } = require('@solana/web3.js');
-const { getAssociatedTokenAddress } = require('@solana/spl-token'); // Required for working with SPL tokens
+const solanaWeb3 = require('@solana/web3.js');
+const splToken = require('@solana/spl-token');
 
-const rpc = 'https://summer-alpha-haze.solana-mainnet.quiknode.pro/07d1622fe7e76082b6263be1c9d35c57f0c11ae3/';
-const connection = new Connection(rpc);
-
-// The wallet public key you want to query
-const wallet = new PublicKey('4v2XqX1CuAtUrzyTBR6qVdmcVdzUJEg5F9tpop5SiWE4');
-
-// The mint address of the SPL token you want to check (example: USDC)
-const tokenMintAddress = new PublicKey('D3QiRT12vKBpj87h99ufQFz4mCpbPC7JVy1U6NRKpump'); // Example: USDC Mint Address
-
-async function getTokenBalance(connection, wallet, tokenMintAddress) {
+const getTokenBalance = async () => {
+    const rpc = 'https://summer-alpha-haze.solana-mainnet.quiknode.pro/07d1622fe7e76082b6263be1c9d35c57f0c11ae3/';
+    const connection = new solanaWeb3.Connection(rpc);
+    const wallet = new solanaWeb3.PublicKey('4v2XqX1CuAtUrzyTBR6qVdmcVdzUJEg5F9tpop5SiWE4');
+    const tokenMintAddress = new solanaWeb3.PublicKey('D3QiRT12vKBpj87h99ufQFz4mCpbPC7JVy1U6NRKpump');
     try {
-        // Get the associated token address for the wallet and token mint address
-        const associatedTokenAccount = await getAssociatedTokenAddress(
-            tokenMintAddress,  // Token Mint Address
-            wallet             // Wallet PublicKey
+        const associatedTokenAccount = await splToken.getAssociatedTokenAddress(
+            tokenMintAddress,
+            wallet         
         );
 
-        // Fetch the balance for the associated token account
         const info = await connection.getTokenAccountBalance(associatedTokenAccount);
         
         if (info.value.uiAmount == null) {
@@ -32,4 +25,4 @@ async function getTokenBalance(connection, wallet, tokenMintAddress) {
     }
 }
 
-getTokenBalance(connection, wallet, tokenMintAddress);
+getTokenBalance();
